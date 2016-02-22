69c4f15b2260cd57a1cfe4e4d15be71783a5d23c
张星宇 <649395594@qq.com>
2016-02-22 16:38:53 +0800

    简单的实现了登陆界面的UI和逻辑

--- UHome/UHome/Common/LoginViewController.swift
+++ UHome/UHome/Common/LoginViewController.swift
@@ -19,7 +19,8 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         view.backgroundColor = "250 251 252".ktcolor
         navigationController?.navigationBar.hidden = true
-        
+        let tapGesture = UITapGestureRecognizer(target: self, action: Selector("closeTextFiled:"))
+        view.addGestureRecognizer(tapGesture)
         /**
         *  logo
         */
@@ -28,9 +29,10 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             make.centerX.equalTo(view.snp_centerX)
             make.width.equalTo(165)
             make.height.equalTo(120)
-            self.topConstraint = make.top.equalTo(20).constraint
+            self.topConstraint = make.top.equalTo(30).constraint
         }
         
+        /// 白色区域
         let whiteContainer = UIView()
         view.addSubview(whiteContainer)
         whiteContainer.snp_makeConstraints { (make) -> Void in
@@ -43,6 +45,20 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         whiteContainer.layer.borderWidth = 0.6
         whiteContainer.layer.borderColor = "203 205 206".ktcolor.CGColor
         
+        /// 分割线
+        let seperateLine = UIView()
+        view.addSubview(seperateLine)
+        seperateLine.snp_makeConstraints { (make) -> Void in
+            make.left.equalTo(20)
+            make.centerX.equalTo(view.snp_centerX)
+            make.height.equalTo(0.6)
+            make.top.equalTo(whiteContainer.snp_top).offset(40)
+        }
+        seperateLine.backgroundColor = "203 205 206".ktcolor
+        
+        /**
+        *  username
+        */
         view.addSubview(usernameField)
         usernameField.snp_makeConstraints { (make) -> Void in
             make.left.equalTo(20)
@@ -82,11 +98,30 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     }
     
     func loginButtonClicked(sender: AnyObject) {
-        self.topConstraint?.updateOffset(50)
-        UIView.animateWithDuration(0.5, animations: { () -> Void in
-            self.view.layoutIfNeeded()
-            }) { (isFinishd: Bool) -> Void in
-                self.navigationController?.pushViewController(MainTabBarController(), animated: false)
+//        self.topConstraint?.updateOffset(50)
+//        UIView.animateWithDuration(0.5, animations: { () -> Void in
+//            self.view.layoutIfNeeded()
+//            }) { (isFinishd: Bool) -> Void in
+//                
+//        }
+        let mainDic = NSDictionary.init(contentsOfFile: NSBundle.mainBundle().pathForResource("Database", ofType: "plist")!)
+        let usersDic = mainDic!["Users"]
+        for var user in usersDic as! NSArray {
+            user = user as! NSDictionary
+            if user.objectForKey("username") as? String == usernameField.text {
+                if user.objectForKey("password") as? String == passwordField.text {
+                    print("密码正确")
+                }
+                else {
+                    print("密码错误")
+                }
+            }
         }
+        self.navigationController?.pushViewController(MainTabBarController(), animated: false)
+    }
+    
+    func closeTextFiled(sender: AnyObject) {
+        usernameField.endEditing(true)
+        passwordField.endEditing(true)
     }
 }
