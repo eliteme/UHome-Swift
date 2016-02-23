1d538ea6580e1a031841b27c28a829e6e709ebaf
Wl201314 <wjl0304@foxmail.com>
2016-02-23 20:02:33 +0800

    先提交一下，搜索栏咋一直没加上去😂

--- UHome/UHome/Search/SearchViewController.swift
+++ UHome/UHome/Search/SearchViewController.swift
@@ -9,34 +9,18 @@
 import UIKit
 import SnapKit
 
-class SearchViewController: UIViewController, UITableViewDelegate{
+class SearchViewController: UIViewController, UITableViewDelegate, UISearchBarDelegate{
 
     var cityTableVC = CityTableViewController()
     var searchBar: UISearchBar?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
         setUpTableView()
+        setUpSearchBar()
     }
 
-    func setUpSearchBar(){
-        
-        searchBar?.placeholder = "输入城市名称"
-        searchBar?.searchBarStyle = UISearchBarStyle.Default
-        searchBar?.backgroundColor = UIColor.blueColor()
-        searchBar?.showsCancelButton = true
-        view.addSubview(searchBar!);
-        searchBar?.snp_makeConstraints(closure: { (make) -> Void in
-            make.top.equalTo(0)
-            make.left.equalTo(5)
-            make.right.equalTo(-5)
-            make.height.equalTo(60)
-        })
-        
-    }
-    
     func setUpTableView(){
         view.addSubview(cityTableVC.view)
         cityTableVC.view.snp_makeConstraints { (make) -> Void in
@@ -45,6 +29,24 @@ class SearchViewController: UIViewController, UITableViewDelegate{
             make.width.equalTo(ScreenWidth)
             make.bottom.equalTo(view).offset(-globalTabbarHeight)
         }
+            print(cityTableVC.view.frame)
+    }
+    
+    func setUpSearchBar(){
         
+        searchBar = UISearchBar()
+        searchBar?.placeholder = "输入城市名称"
+        searchBar?.searchBarStyle = UISearchBarStyle.Default
+        searchBar?.backgroundColor = UIColor.blueColor()
+        searchBar?.showsCancelButton = true
+        searchBar!.delegate = self
+        view.addSubview(searchBar!);
+        searchBar?.snp_makeConstraints(closure: { (make) -> Void in
+            make.top.equalTo(0)
+            make.left.equalTo(0)
+            make.width.equalTo(ScreenWidth)
+            make.height.equalTo(60)
+        })
+        print(searchBar?.frame);
     }
 }
