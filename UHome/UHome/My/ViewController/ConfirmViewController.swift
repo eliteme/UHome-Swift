d9e559dfa175c1338fceed4a622aa4060d90d513
张星宇 <649395594@qq.com>
2016-02-24 09:15:48 +0800

    早起交一波，待会儿去坐火车了。改了一下布局，添加了一些图片

--- UHome/UHome/My/ViewController/ConfirmViewController.swift
+++ UHome/UHome/My/ViewController/ConfirmViewController.swift
@@ -21,30 +21,9 @@ class ConfirmViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureConfirmation()
+        setupView()
         
-        view.addSubview(realPhotoConfirmArea)
-        realPhotoConfirmArea.snp_makeConstraints { (make) -> Void in
-            make.top.left.equalTo(view).offset(-0.3)
-            make.centerX.equalTo(view)
-            make.height.equalTo(view).multipliedBy(1.0 / 3.0)
-        }
-        realPhotoConfirmArea.layer.borderWidth = 0.3
-        realPhotoConfirmArea.layer.borderColor = "203 205 206".ktcolor.CGColor
-        realPhotoConfirmArea.backgroundColor = "250 251 252".ktcolor
-        
-        /**
-        实名认证区域
-        */
-        view.addSubview(realNameConfirmArea)
-        realNameConfirmArea.snp_makeConstraints { (make) -> Void in
-            make.left.equalTo(view).offset(-0.3)
-            make.top.equalTo(realPhotoConfirmArea.snp_bottom)
-            make.centerX.equalTo(view)
-            make.height.equalTo(view).multipliedBy(1.0 / 3.0)
-        }
-        realNameConfirmArea.layer.borderWidth = 0.3
-        realNameConfirmArea.layer.borderColor = "203 205 206".ktcolor.CGColor
-        realNameConfirmArea.backgroundColor = "250 251 252".ktcolor
+        self.view.backgroundColor = UIColor.whiteColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -67,4 +46,65 @@ class ConfirmViewController: UIViewController {
         }
     }
 
+}
+
+extension ConfirmViewController {
+    func setupView() {
+        let clickImage = UIImage(named: "click")
+        let clickImageView1 = UIImageView(image: clickImage)
+        let clickImageView2 = UIImageView(image: clickImage)
+        let clickImageView3 = UIImageView(image: clickImage)
+        
+        view.addSubview(realPhotoConfirmArea)
+        realPhotoConfirmArea.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(view).offset(-0.3).offset(64)
+            make.left.equalTo(view).offset(-0.3)
+            make.centerX.equalTo(view)
+            make.height.equalTo(view).offset(-37).multipliedBy(1.0 / 3.0)
+        }
+        realPhotoConfirmArea.layer.borderWidth = 0.3
+        realPhotoConfirmArea.layer.borderColor = "203 205 206".ktcolor.CGColor
+        realPhotoConfirmArea.backgroundColor = "250 251 252".ktcolor
+        
+        if let photoName = realPhotoName {
+            realPhotoConfirmArea.addSubview(clickImageView1)
+            clickImageView1.snp_makeConstraints(closure: { (make) -> Void in
+                make.centerY.equalTo(realPhotoConfirmArea.snp_centerY)
+                make.left.equalTo(20)
+                make.width.height.equalTo(100)
+            })
+            
+            let realPhoto = UIImageView(image: UIImage(named: photoName))
+            realPhotoConfirmArea.addSubview(realPhoto)
+            realPhoto.snp_makeConstraints(closure: { (make) -> Void in
+                make.centerY.equalTo(realPhotoConfirmArea.snp_centerY)
+                make.right.equalTo(realPhotoConfirmArea.snp_right).offset(-30)
+                make.width.height.equalTo(150)
+            })
+        }
+        /**
+        实名认证区域
+        */
+        view.addSubview(realNameConfirmArea)
+        realNameConfirmArea.snp_makeConstraints { (make) -> Void in
+            make.left.equalTo(view).offset(-0.3)
+            make.top.equalTo(realPhotoConfirmArea.snp_bottom)
+            make.centerX.equalTo(view)
+            make.height.equalTo(realPhotoConfirmArea)
+        }
+        realNameConfirmArea.layer.borderWidth = 0.3
+        realNameConfirmArea.layer.borderColor = "203 205 206".ktcolor.CGColor
+        realNameConfirmArea.backgroundColor = "250 251 252".ktcolor
+        
+        view.addSubview(realPhoneConfirmArea)
+        realPhoneConfirmArea.snp_makeConstraints { (make) -> Void in
+            make.left.equalTo(view).offset(-0.3)
+            make.top.equalTo(realNameConfirmArea.snp_bottom)
+            make.centerX.equalTo(view)
+            make.height.equalTo(realPhotoConfirmArea)
+        }
+        realPhoneConfirmArea.layer.borderWidth = 0.3
+        realPhoneConfirmArea.layer.borderColor = "203 205 206".ktcolor.CGColor
+        realPhoneConfirmArea.backgroundColor = "250 251 252".ktcolor
+    }
 }
